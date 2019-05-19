ada4d47b9a7106e99a2007272696ddda75859fb9
mjhorvath <17457030+mjhorvath@users.noreply.github.com>
2019-05-19 08:47:05 -0400

    Tweak dimensions and colors of graphical projection comparison images.

---
+++ Projections/graphical_projection_comparison/dark/combine.cmd
@@ -0,0 +1,18 @@
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison00.png -page +0+0 ../markers/markers_00.png -layers flatten combined_00.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison01.png -page +0+0 ../markers/markers_01.png -layers flatten combined_01.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison02.png -page +0+0 ../markers/markers_02.png -layers flatten combined_02.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison03.png -page +0+0 ../markers/markers_03.png -layers flatten combined_03.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison04.png -page +0+0 ../markers/markers_04.png -layers flatten combined_04.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison05.png -page +0+0 ../markers/markers_05.png -layers flatten combined_05.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison06.png -page +0+0 ../markers/markers_06.png -layers flatten combined_06.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison07.png -page +0+0 ../markers/markers_07.png -layers flatten combined_07.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison08.png -page +0+0 ../markers/markers_08.png -layers flatten combined_08.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison09.png -page +0+0 ../markers/markers_09.png -layers flatten combined_09.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison10.png -page +0+0 ../markers/markers_10.png -layers flatten combined_10.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison11.png -page +0+0 ../markers/markers_11.png -layers flatten combined_11.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison12.png -page +0+0 ../markers/markers_12.png -layers flatten combined_12.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison13.png -page +0+0 ../markers/markers_13.png -layers flatten combined_13.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison14.png -page +0+0 ../markers/markers_14.png -layers flatten combined_14.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison15.png -page +0+0 ../markers/markers_15.png -layers flatten combined_15.png
+"C:\Program Files\ImageMagick-6.9.2-Q16\convert.exe" -size 1500x1500 -page +0+0 graphical_projection_comparison16.png -page +0+0 ../markers/markers_16.png -layers flatten combined_16.png
+pause
